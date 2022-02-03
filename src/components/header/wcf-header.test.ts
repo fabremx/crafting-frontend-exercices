fe018b8b8fda44cefee55230a26933cc519e4a42
Maxime Fabre <fabre.m96@gmail.com>
2022-02-03 11:54:53 +0100

    Change header test

--- src/components/header/wcf-header.test.ts
+++ src/components/header/wcf-header.test.ts
@@ -2,5 +2,5 @@ import { Header } from "./wcf-header";
 
 it('should return title header', () => {
     const header = new Header();
-    expect(header.shadowRoot?.querySelector('#header')).toMatchSnapshot()
+    expect(header.shadowRoot?.querySelector('.header__title')?.textContent).toBe('Web Components Bets Application')
 });
--- src/components/header/wcf-header.ts
+++ src/components/header/wcf-header.ts
@@ -4,7 +4,7 @@ const template = document.createElement('template');
 template.innerHTML = `
 <style>${css}</style>
 <div id="header">
-    <h3>Web Components Bets Application</h3>
+    <h3 class="header__title">Web Components Bets Application</h3>
 </div>
 `;
 
