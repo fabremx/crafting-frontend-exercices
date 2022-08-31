67110cd6f355ae5877219b030e8ed518bd992175
Skander Besbes <besbes.skander@gmail.com>
2022-08-31 13:36:46 +0200

    Add storybooks for footer
    Add footer story

--- src/components/footer/footer.stories.ts
+++ src/components/footer/footer.stories.ts
@@ -1,9 +1,15 @@
-import './footer'
-
 export default {
     title: 'Components/Footer',
+    argTypes: {
+        isUserConnected: {
+            control: 'boolean',
+        },
+    }
 }
+
+const Template = (argTypes: any) => `<arl-footer is-user-connected="${argTypes.isUserConnected}"></arl-footer>`
 
 export const Default = () => '<arl-footer></arl-footer>'
 export const FooterWithConnectedUser = () => '<arl-footer is-user-connected="true"></arl-footer>'
 export const FooterWithDisconnectedUser = () => '<arl-footer is-user-connected="false"></arl-footer>'
+export const Default = Template.bind({});
